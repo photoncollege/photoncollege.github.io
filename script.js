01ad587507f95f84f2a0df53f66fe21d31cceb77
photoncollege <photonacademy1@gmail.com>
2024-09-16 19:33:35 +0400

    mp

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// script.js
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
 
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-  document.querySelector('.nav-wrap').classList.toggle('open');
-});
+    if (!navLinks.classList.contains('open')) {
+        navLinks.style.height = 'auto';
+        const height = navLinks.scrollHeight + 'px';
+        navLinks.style.height = '0';
+
+        setTimeout(() => {
+            navLinks.style.height = height;
+        }, 10);
+
+        navLinks.classList.add('open');
+        navLinks.style.opacity = '1';
+    } else {
+        navLinks.style.height = navLinks.scrollHeight + 'px';
+        setTimeout(() => {
+            navLinks.style.height = '0';
+        }, 10);
+        
+        navLinks.classList.remove('open');
+        navLinks.style.opacity = '0';
+    }
+}
