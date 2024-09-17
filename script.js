9b64bad47b21cf1a86bfc2efb0efe93420aabfd7
photoncollege <photonacademy1@gmail.com>
2024-09-18 00:39:36 +0400

    7a

--- script.js
+++ script.js
@@ -2,23 +2,20 @@ function toggleMenu() {
     const navLinks = document.getElementById('nav-links');
 
     if (!navLinks.classList.contains('open')) {
-        navLinks.style.height = 'auto';
         const height = navLinks.scrollHeight + 'px';
-        navLinks.style.height = '0';
-
-        setTimeout(() => {
-            navLinks.style.height = height;
-        }, 10);
+        navLinks.style.height = height;
 
         navLinks.classList.add('open');
         navLinks.style.opacity = '1';
     } else {
-        navLinks.style.height = navLinks.scrollHeight + 'px';
-        setTimeout(() => {
-            navLinks.style.height = '0';
-        }, 10);
-        
+        navLinks.style.height = '0';
         navLinks.classList.remove('open');
         navLinks.style.opacity = '0';
     }
 }
+
+window.onload = function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.style.height = '0';
+    navLinks.style.opacity = '0';
+};
